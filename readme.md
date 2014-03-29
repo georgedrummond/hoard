117365688437c1b3fe2a38c615db50331a62cdda
George Drummond <georgedrummond@gmail.com>
2014-03-29 14:37:23 +0000

    Add a logo to the readme and some cleanup

--- readme.md
+++ readme.md
@@ -1,3 +1,5 @@
 [![Build Status](https://travis-ci.org/georgedrummond/hoard.svg?branch=master)](https://travis-ci.org/georgedrummond/hoard)
 
+![Rentify](https://github.com/georgedrumond/hoard/raw/master/app/assets/images/logo.png)
+
 # Hoard is a work in progress!
--- spec/controllers/api/v1/rubygems_controller_spec.rb
+++ spec/controllers/api/v1/rubygems_controller_spec.rb
@@ -1,5 +1,28 @@
 require 'spec_helper'
 
 describe Api::V1::RubygemsController do
+  let!(:gem_package) { 'mafia-0.1.4.gem' }
+  let!(:file)        { fixture_file_upload gem_package, 'mafia-0.1.4.gem' }
 
+  it 'has authentication'
+
+  describe '#create' do
+    context 'success' do
+      before do
+        Upload.any_instance.stub(:store!).and_return(true)
+        post :create, file: file
+      end
+
+      it { should respond_with(:created) }
+    end
+
+    context 'failure' do
+      before do
+        Upload.any_instance.stub(:store!).and_return(false)
+        post :create, file: file
+      end
+
+      it { should respond_with(:conflict) }
+    end
+  end
 end
