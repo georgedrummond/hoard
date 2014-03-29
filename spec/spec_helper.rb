d9edd5ae44cc6a6e7d9873659e3186225a5f862d
George Drummond <georgedrummond@gmail.com>
2014-03-29 12:40:09 +0000

    Set the organisation we expect a user to be in for our specs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,6 +5,8 @@ require 'rspec/rails'
 require 'rspec/autorun'
 require 'vcr'
 
+ENV['GITHUB_ORGANIZATION_ID'] = 'rentify'
+
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
 Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
