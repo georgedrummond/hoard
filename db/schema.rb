4ec6fd19dbd76dd22f73b4cf9016ec09897675ac
George Drummond <georgedrummond@gmail.com>
2014-03-30 12:02:14 +0100

    Use postgres instead of sqlite3

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20140329015543) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "releases", force: true do |t|
     t.integer  "rubygem_id"
     t.string   "name"
